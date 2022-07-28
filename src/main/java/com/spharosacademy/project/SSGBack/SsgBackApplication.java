e55f819f21423a9646c83d059f4cf5e2035616cb
ChanYoungHong <cksdud9883@naver.com>
2022-07-28 17:44:29 +0900

    백엔드 기초세팅 커밋

---
+++ src/main/java/com/spharosacademy/project/SSGBack/SsgBackApplication.java
@@ -0,0 +1,13 @@
+package com.spharosacademy.project.SSGBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsgBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsgBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spharosacademy/project/SSGBack/SsgBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spharosacademy.project.SSGBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsgBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
