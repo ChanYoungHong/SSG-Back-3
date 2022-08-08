54fe5d150f8b83b4339848f48e284e9c0450f79d
sunju <sunju.nam118@gmail.com>
2022-08-08 12:08:45 +0900

    qna update

--- src/main/java/com/spharosacademy/project/SSGBack/Answer/repo/AnswerRepo.java
+++
@@ -1,7 +0,0 @@
-package com.spharosacademy.project.SSGBack.Answer.repo;
-
-import com.spharosacademy.project.SSGBack.Answer.entity.Answer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AnswerRepo extends JpaRepository<Answer, Integer> {
-}
