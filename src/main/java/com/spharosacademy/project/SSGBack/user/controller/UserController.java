9e653ab93d5850149a25ee976ea9eb59df45a08b
ChanYoungHong <cksdud9883@naver.com>
2022-08-16 17:29:03 +0900

    crossOrigin 추가

--- src/main/java/com/spharosacademy/project/SSGBack/user/controller/UserController.java
+++ src/main/java/com/spharosacademy/project/SSGBack/user/controller/UserController.java
@@ -7,6 +7,7 @@ import com.spharosacademy.project.SSGBack.user.service.UserService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/user")
 @RequiredArgsConstructor
 public class UserController {
