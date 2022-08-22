1480f1b93459a2e2f9aba97262913f03975045bd
sunju <sunju.nam118@gmail.com>
2022-08-22 14:40:23 +0900

    empty

--- src/main/java/com/spharosacademy/project/SSGBack/Product/repository/CategoryLRepo.java
+++
@@ -1,7 +0,0 @@
-package com.spharosacademy.project.SSGBack.Product.repository;
-
-import com.spharosacademy.project.SSGBack.Product.domain.CategoryL;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryLRepo extends JpaRepository<CategoryL, Integer> {
-}
