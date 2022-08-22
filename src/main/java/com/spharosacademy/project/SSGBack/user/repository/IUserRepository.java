98c0351af4d75228d6b59845bcbf1fa19af0433e
mins00oo <alstn7223@gmail.com>
2022-08-22 14:46:46 +0900

    merge합시다

--- src/main/java/com/spharosacademy/project/SSGBack/user/repository/IUserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.spharosacademy.project.SSGBack.user.repository;
-
-import com.spharosacademy.project.SSGBack.user.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IUserRepository extends JpaRepository<User, Long> {
-}
